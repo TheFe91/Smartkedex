48512ecb70859481006f9835740e6671898a5e8c
Alessandro Defendenti <alessandrodefendenti@gmail.com>
2016-10-03 10:46:23 +0200

    Aggiunto bottone back in ActivityBar; iniziati i lavori di costruzione dell'interfaccia Pokedetails

--- app/src/main/java/com/example/thefe/newsmartkedex/PokemonDetails.java
+++ app/src/main/java/com/example/thefe/newsmartkedex/PokemonDetails.java
@@ -1,7 +1,7 @@
 package com.example.thefe.newsmartkedex;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -11,18 +11,24 @@ import android.widget.ImageView;
  * Created by TheFe on 02/10/2016.
  */
 
-public class PokemonDetails extends Activity {
+public class PokemonDetails extends AppCompatActivity {
     @Override
     public void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pokedetails);
 
+        getActionBar();
+
         setInitialImage();
     }
 
     private void setInitialImage () {
-        ImageView imageView = (ImageView) findViewById(R.id.prova);
-        imageView.setImageResource(R.drawable.pkmn102);
+        ImageView imageView = (ImageView) findViewById(R.id.tmpPkmn);
+        imageView.setImageResource(R.drawable.pkmn);
+        imageView = (ImageView) findViewById(R.id.tipo1);
+        imageView.setImageResource(R.drawable.tipo);
+        imageView = (ImageView) findViewById(R.id.tipo2);
+        imageView.setImageResource(R.drawable.tipo);
     }
 
     public void goMain (View view) {
