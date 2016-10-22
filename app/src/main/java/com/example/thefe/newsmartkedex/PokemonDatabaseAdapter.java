595abdf4435bbc27c0f655f6215ae5f0d734ba7f
Alessandro Defendenti <alessandrodefendenti@gmail.com>
2016-10-22 16:09:37 +0200

    Database works almost done

--- app/src/main/java/com/example/thefe/newsmartkedex/PokemonDatabaseAdapter.java
+++ app/src/main/java/com/example/thefe/newsmartkedex/PokemonDatabaseAdapter.java
@@ -21,7 +21,7 @@ class PokemonDatabaseAdapter {
 
     void insertData(String owner, String smartkedex, String language, int pokemonGO) {
         SQLiteDatabase db = helper.getWritableDatabase();
-        db.execSQL("INSERT INTO Settings (Owner, Smartkedex, Language, PokemonGO) VALUES ('"+owner+"', '"+smartkedex+"', '"+language+"', '"+pokemonGO+"')");
+        db.execSQL("INSERT INTO Settings (Owner, SmartkedexName, Language, PokemonGO) VALUES ('"+owner+"', '"+smartkedex+"', '"+language+"', '"+pokemonGO+"')");
     }
 
     int getRows () {
@@ -64,7 +64,7 @@ class PokemonDatabaseAdapter {
     String getSmartkedex () {
         String smartkedex = "";
         SQLiteDatabase db = helper.getReadableDatabase();
-        String[] columns = {PokemonHelper.SMARTKEDEX};
+        String[] columns = {PokemonHelper.SMARTKEDEXNAME};
         Cursor cursor = db.query(PokemonHelper.SETTINGS, columns, null, null, null, null, null);
 
         while (cursor.moveToNext()) {
@@ -99,7 +99,7 @@ class PokemonDatabaseAdapter {
 
     void updateSmartkedex (String newSmartkedex, String oldSmartkedex) {
         SQLiteDatabase db = helper.getWritableDatabase();
-        db.execSQL("UPDATE Settings SET Smartkedex = '"+newSmartkedex+"' WHERE Smartkedex = '"+oldSmartkedex+"';");
+        db.execSQL("UPDATE Settings SET SmartkedexName = '"+newSmartkedex+"' WHERE SmartkedexName = '"+oldSmartkedex+"';");
     }
 
     void updatePokemonGO (int newPokemonGO, int oldPokemonGO) {
@@ -110,37 +110,125 @@ class PokemonDatabaseAdapter {
 
     private static class PokemonHelper extends SQLiteOpenHelper {
         private static final String DATABASE_NAME = "PokemonDatabase.db";
-        private static final int DATABASE_VERSION = 15;
+        private static final int DATABASE_VERSION = 19;
 
+        //Types Declaration
         private static final String VARCHAR = " VARCHAR(";
+        private static final String INT = " INT(";
+        private static final String FLOAT = " FLOAT(";
 
+        //Tables Declaration
         private static final String SETTINGS = "Settings";
+        private static final String POKEMON = "Pokemon";
+        private static final String ATTACK = "Attack";
+        private static final String ULTI = "Ulti";
+        private static final String TYPE = "Type";
+        private static final String CATCHES = "Catches";
+        private static final String USER = "User";
+
+        //Columns Declaration
         private static final String LANGUAGE = "Language";
         private static final String OWNER = "Owner";
-        private static final String SMARTKEDEX = "Smartkedex";
+        private static final String SMARTKEDEXNAME = "SmartkedexName";
         private static final String POKEMONGO = "PokemonGO";
+        private static final String ID = "ID";
+        private static final String POKEMONNAME = "PokemonName";
+        private static final String ULTI_NAME = "UltiName";
+        private static final String ATTACK_NAME = "AttackName";
+        private static final String DAMAGE_DEALT = "DamageDealt";
+        private static final String DURATION = "Duration";
+        private static final String CRITICAL_CHANCE = "CriticalChance";
+        private static final String TYPE_NAME = "TypeName";
+        private static final String QUANTITY = "Quantity";
+        private static final String EMAIL = "Email";
+        private static final String NAME = "Name";
+        private static final String SURNAME = "Surname";
+        private static final String USERNAME = "Username";
+        private static final String PASSWORD = "Password";
+        private static final String CITY = "City";
+        private static final String POSTALCODE = "PostalCode";
+        private static final String ADDRESS = "Address";
+        private static final String PROVINCE = "Province";
 
-        private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + SETTINGS + "(" + LANGUAGE + VARCHAR + "3), " + OWNER + VARCHAR + "20), " + SMARTKEDEX + VARCHAR + "20) PRIMARY KEY, " + POKEMONGO + " INT(1))";
+
+        //CREATE TABLE Statements
+        private static final String CREATE_SETTINGS = "CREATE TABLE IF NOT EXISTS " + SETTINGS + "(" +
+                                                       LANGUAGE + VARCHAR + "3), " +
+                                                       OWNER + VARCHAR + "20), " +
+                                                       SMARTKEDEXNAME + VARCHAR + "20), " +
+                                                       POKEMONGO + INT + "1), " +
+                                                       EMAIL + VARCHAR + "40) PRIMARY KEY, " +
+                                                       "FOREIGN KEY ("+ EMAIL +") REFERENCES " + USER + " (" + EMAIL + "))";
+
+        private static final String CREATE_POKEMON = "CREATE TABLE IF NOT EXISTS " + POKEMON + "(" +
+                                                      POKEMONNAME + VARCHAR + "20), " +
+                                                      ID + INT + "3) PRIMARY KEY, " +
+                                                      ATTACK_NAME + VARCHAR + "15), " +
+                                                      ULTI_NAME + VARCHAR + "15), " +
+                                                      "FOREIGN KEY (" + ULTI_NAME + ") REFERENCES " + ULTI + " ("+ ULTI_NAME +"), " +
+                                                      "FOREIGN KEY (" + ATTACK_NAME + ") REFERENCES " + ATTACK + "(" + ATTACK_NAME + "))";
+
+        private static final String CREATE_TYPE = "CREATE TABLE IF NOT EXISTS " + TYPE + "(" +
+                                                   TYPE_NAME + VARCHAR + "10) PRIMARY KEY)";
+
+        private static final String CREATE_ULTI = "CREATE TABLE IF NOT EXISTS " + ULTI + "(" +
+                                                   ULTI_NAME + VARCHAR + "15) PRIMARY KEY, " +
+                                                   DAMAGE_DEALT + INT + "3), " +
+                                                   DURATION + FLOAT + "3,2), " +
+                                                   CRITICAL_CHANCE + INT + "2), " +
+                                                   TYPE_NAME + VARCHAR + "10), " +
+                                                   "FOREIGN KEY (" + TYPE_NAME + ") REFERENCES " + TYPE + " (" + TYPE_NAME + "))";
+
+        private static final String CREATE_ATTACK = "CREATE TABLE IF NOT EXISTS " + ATTACK + "(" +
+                                                     ATTACK_NAME + VARCHAR + "15) PRIMARY KEY, " +
+                                                     DAMAGE_DEALT + INT + "3), " +
+                                                     DURATION + FLOAT + "3,2), " +
+                                                     TYPE_NAME + VARCHAR + "10), " +
+                                                     "FOREIGN KEY (" + TYPE_NAME + ") REFERENCES " + TYPE + " (" + TYPE_NAME + "))";
+
+        private static final String CREATE_USER = "CREATE TABLE IF NOT EXISTS " + USER + "(" +
+                                                   EMAIL + VARCHAR + "40) PRIMARY KEY, " +
+                                                   NAME + VARCHAR + "15), " +
+                                                   SURNAME + VARCHAR + "15), " +
+                                                   USERNAME + VARCHAR + "15), " +
+                                                   PASSWORD + VARCHAR + "16), " +
+                                                   ADDRESS + VARCHAR + "50), " +
+                                                   POSTALCODE + VARCHAR + "10), " +
+                                                   PROVINCE + VARCHAR + "7), " +
+                                                   CITY + VARCHAR + "15))";
+
+        private static final String CREATE_CATCHES = "CREATE TABLE IF NOT EXISTS " + CATCHES + "(" +
+                                                      QUANTITY + INT + "2), " +
+                                                      ID + INT + "3), " +
+                                                      EMAIL + VARCHAR + "40), " +
+                                                      "PRIMARY KEY (" + EMAIL + ", " + ID + "), " +
+                                                      "FOREIGN KEY (" + EMAIL + ") REFERENCES " + USER + " (" + EMAIL + "), " +
+                                                      "FOREIGN KEY (" + ID + ") REFERENCES " + POKEMON + " (" + ID + "))";
+
+
 
         private Context context;
 
         PokemonHelper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
             this.context = context;
-            System.out.println(CREATE_TABLE);
-            Toast.makeText(context, "Constructor called", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(context, "Constructor called", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onCreate(SQLiteDatabase db) {
-            db.execSQL(CREATE_TABLE);
+            db.execSQL(CREATE_USER);
+            db.execSQL(CREATE_SETTINGS);
+            db.execSQL(CREATE_TYPE);
+            db.execSQL(CREATE_ATTACK);
+            db.execSQL(CREATE_ULTI);
+            db.execSQL(CREATE_POKEMON);
+            db.execSQL(CREATE_CATCHES);
             Toast.makeText(context, "onCreate called", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            db.execSQL("ALTER TABLE Settings ADD COLUMN PokemonGO INT(1)");
-            db.execSQL("UPDATE Settings SET PokemonGO=0");
             onCreate(db);
             Toast.makeText(context, "onUpgrade called", Toast.LENGTH_SHORT).show();
         }
