904a647a155204256e20c191d6aa9e607354b7fb
TheFe91 <alessandrodefendenti@gmail.com>
2017-01-21 18:36:26 +0100

    removed for now apprater from main

--- app/src/main/java/com/example/thefe/newsmartkedex/MainActivity.java
+++ app/src/main/java/com/example/thefe/newsmartkedex/MainActivity.java
@@ -29,10 +29,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        AppRater appRater = new AppRater(this);
-        appRater.setLaunchesBeforePrompt(3);
-        appRater.setPhrases("Dacci un Feedback", "Ti è piaciuta la nostra app? Dedicaci un paio di minuti e dacci un feedback sul Play Store", "FEEDBACK", "DOPO", "MAI");
-        appRater.demo();
+        //AppRater appRater = new AppRater(this);
+        //appRater.setLaunchesBeforePrompt(3);
+        //appRater.setPhrases("Dacci un Feedback", "Ti è piaciuta la nostra app? Dedicaci un paio di minuti e dacci un feedback sul Play Store", "FEEDBACK", "DOPO", "MAI");
+        //appRater.demo();
     }
 
     @Override
