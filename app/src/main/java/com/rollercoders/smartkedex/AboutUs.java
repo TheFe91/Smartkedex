28d4c785d58f27f1caf8a72a3a75bde48ebd38e3
TheFe91 <alessandrodefendenti@gmail.com>
2017-01-23 21:21:34 +0100

    re-edited .gitignore; fixed contact us form not working

--- app/src/main/java/com/rollercoders/smartkedex/AboutUs.java
+++ app/src/main/java/com/rollercoders/smartkedex/AboutUs.java
@@ -1,10 +1,17 @@
 package com.rollercoders.smartkedex;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
 
 /**
  * Created by TheFe on 17/10/2016.
@@ -23,11 +30,22 @@ public class AboutUs extends AppCompatActivity {
         contact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mailClient = new Intent();
-                mailClient.setAction(Intent.ACTION_SEND);
-                mailClient.putExtra(Intent.EXTRA_EMAIL, "rollercodersteam@gmail.com");
+                Intent mailClient = new Intent(Intent.ACTION_SEND);
+                mailClient.setData(Uri.parse("mailto:"));
                 mailClient.setType("text/plain");
-                startActivity(mailClient);
+                mailClient.putExtra(Intent.EXTRA_EMAIL, new String[] {"rollercodersteam@gmail.com"});
+
+                PackageManager packageManager = getPackageManager();
+                List<ResolveInfo> activities = packageManager.queryIntentActivities(mailClient, 0);
+
+                if (activities.size() > 0) {
+                    String title = "Completa azione con...";
+                    Intent chooser = Intent.createChooser(mailClient, title);
+                    startActivity(chooser);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "There are no email clients installed", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
