27a6e10f2dd5046c2bf482b71cce9aff13872cc8
Alessandro Defendenti <alessandrodefendenti@gmail.com>
2017-02-23 12:07:50 +0100

    added RollerCoders logo on SplashActivity; general SplashActivity fix

--- app/src/main/java/com/rollercoders/smartkedex/SplashActivity.java
+++ app/src/main/java/com/rollercoders/smartkedex/SplashActivity.java
@@ -3,6 +3,7 @@ package com.rollercoders.smartkedex;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 /**
  * Created by TheFe on 03/11/2016.
@@ -13,15 +14,31 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
 
-        Intent i = new Intent(this, Welcome.class);
-        try {
-            Thread.sleep(2000);
-        }
-        catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        startActivity(i);
+        ImageView imageView = (ImageView)findViewById(R.id.splash);
+        imageView.setImageResource(R.drawable.splashlogo);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this, Welcome.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
         finish();
     }
+
 }
