059f0fdadd9cdc7c57daa225b7335f413c96769f
TheFe91 <alessandrodefendenti@gmail.com>
2017-06-11 16:04:36 +0200

    fixed notification when app is outdated

--- app/src/main/java/com/rollercoders/smartkedex/InitialLogin.java
+++ app/src/main/java/com/rollercoders/smartkedex/InitialLogin.java
@@ -2,17 +2,22 @@ package com.rollercoders.smartkedex;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by TheFe on 15/05/2017.
@@ -29,29 +34,25 @@ public class InitialLogin extends Activity {
         final PokemonDatabaseAdapter pokemonDatabaseAdapter = new PokemonDatabaseAdapter(this);
 
         if (!pokemonDatabaseAdapter.getAppVersion()) {
-            final AlertDialog.Builder builder;
-            if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-                builder = new AlertDialog.Builder(context, android.R.style.Theme_Dialog);
-            else
-                builder = new AlertDialog.Builder(context);
-                builder.setTitle(getResources().getString(getResources().getIdentifier("versionFail", "string", getPackageName())))
-                    .setMessage(getResources().getString(getResources().getIdentifier("versionFailMessage", "string", getPackageName())))
-                    .setPositiveButton(getResources().getString(getResources().getIdentifier("OK", "string", getPackageName())), new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            final String appPackageName = getPackageName();
-                            try {
-                                dialogInterface.dismiss();
-                                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id="+appPackageName)));
-                            } catch (android.content.ActivityNotFoundException e) {
-                                dialogInterface.dismiss();
-                                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id="+appPackageName)));
-                            }
-                        }
-                    })
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .show();
-            finish();
+            Toast.makeText(this, getResources().getString(getResources().getIdentifier("versionFailMessage", "string", getPackageName())), Toast.LENGTH_LONG).show();
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                    .setSmallIcon(android.R.drawable.ic_dialog_alert)
+                    .setContentTitle(getResources().getString(getResources().getIdentifier("versionFail", "string", getPackageName())))
+                    .setContentText(getResources().getString(getResources().getIdentifier("versionFailMessage", "string", getPackageName())));
+
+            final String appPackageName = getPackageName();
+            Intent resultIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id="+appPackageName));
+
+            PendingIntent contentIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            builder.setContentIntent(contentIntent);
+            NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(0, builder.build());
+
+            Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+            homeIntent.addCategory(Intent.CATEGORY_HOME);
+            homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(homeIntent);
         }
 
         if (pokemonDatabaseAdapter.getRememberME() == 1) {
